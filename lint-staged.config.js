ee2787bf1ec5636f7ca0dd3f4b0ee2a6078f53fc
Guillaume Chau <guillaume.b.chau@gmail.com>
2025-03-19 16:37:29 +0100

    chore: improve lint-staged config

---
+++ lint-staged.config.js
@@ -0,0 +1,40 @@
+import fs from 'node:fs'
+import { fileURLToPath } from 'node:url'
+import path from 'pathe'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+/**
+ * @filename: lint-staged.config.js
+ * @type {import('lint-staged').Configuration}
+ */
+export default {
+  '*': [
+    'eslint --fix',
+  ],
+  'docs/**': _stagedFiles => [
+    'pnpm run docs:build',
+  ],
+  'packages/**': (stagedFiles) => {
+    const packages = stagedFiles.map((file) => {
+      const packagePath = path.relative(__dirname, file)
+      return packagePath.split('/')[1]
+    })
+    const packageNames = [...new Set(packages)].map((packagePath) => {
+      const packageJsonPath = path.join(__dirname, 'packages', packagePath, 'package.json')
+      const content = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'))
+      return content.name
+    })
+    return [
+      'pnpm run test',
+      `pnpm run -r ${packageNames.map(name => `--filter ...${name}`).join(' ')} build`,
+      `pnpm run -r ${packageNames.map(name => `--filter ...${name}`).join(' ')} test:types`,
+    ]
+  },
+  'README.md': _stagedFiles => [
+    'pnpm run copy-readme',
+  ],
+  'package.json|pnpm-lock.yaml': _stagedFiles => [
+    'run-p docs:build bb test',
+  ],
+}
