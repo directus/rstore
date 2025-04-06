3a47a0a76a045ab6f4f9942f0704f0728edfc6bf
Guillaume Chau <guillaume.b.chau@gmail.com>
2025-04-06 23:29:39 +0200

    fix(drizzle): no key found

--- packages/nuxt-drizzle/src/module.ts
+++ packages/nuxt-drizzle/src/module.ts
@@ -177,7 +177,7 @@ export default defineNuxtModule<ModuleOptions>({
           scopeId: 'rstore-drizzle',
           meta: {
             table: tableName,
-            primaryKeys: (config as any)?.primaryKeys?.length ? (config as any).primaryKeys : (config?.columns as any[] | undefined)?.filter(col => col.primary).map(col => col.keyAsName ? col.key : col.name),
+            primaryKeys: (config as any)?.primaryKeys?.length ? (config as any).primaryKeys : (config?.columns as any[] | undefined)?.filter(col => col.primary).map(col => col.keyAsName ? col.key ?? col.name : col.name),
           },
         }
         models.push(model)
