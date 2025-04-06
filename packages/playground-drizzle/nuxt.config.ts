a546812a169c5eb28ad80dad7033c0ed8b536c33
Guillaume Chau <guillaume.b.chau@gmail.com>
2025-04-06 23:26:41 +0200

    chore: drizzle playground devtools

--- packages/playground-drizzle/nuxt.config.ts
+++ packages/playground-drizzle/nuxt.config.ts
@@ -1,4 +1,5 @@
-import { createResolver } from 'nuxt/kit'
+import { startSubprocess } from '@nuxt/devtools-kit'
+import { createResolver, defineNuxtModule } from 'nuxt/kit'
 
 const { resolve } = createResolver(import.meta.url)
 
@@ -6,8 +7,34 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/ui',
     // '@rstore/nuxt-drizzle',
+    '../nuxt/src',
     '../nuxt-drizzle/src',
     '@vueuse/nuxt',
+
+    /**
+     * Start a sub Nuxt Server for developing the client
+     *
+     * The terminal output can be found in the Terminals tab of the devtools.
+     */
+    defineNuxtModule({
+      setup(_, nuxt) {
+        if (!nuxt.options.dev) {
+          return
+        }
+
+        const _process = startSubprocess(
+          {
+            command: 'npx',
+            args: ['nuxi', 'dev', '--port', '3300'],
+            cwd: resolve(__dirname, '../nuxt/client'),
+          },
+          {
+            id: 'rstore:client',
+            name: 'rstore Client Dev',
+          },
+        )
+      },
+    }),
   ],
   devtools: { enabled: true },
   compatibilityDate: '2024-12-19',
