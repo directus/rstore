2cc2a581d2d46082ac7e1619f6dcf24b19abb63a
Guillaume Chau <guillaume.b.chau@gmail.com>
2025-04-11 20:45:33 +0200

    feat(nuxt-drizzle): support with, columns, top-level orderBy (#36)

--- packages/nuxt-drizzle/src/runtime/server/api/[key]/index.get.ts
+++ packages/nuxt-drizzle/src/runtime/server/api/[key]/index.get.ts
@@ -1,10 +1,17 @@
-import { defineEventHandler, getRouterParams } from 'h3'
-import { getDrizzleKeyWhere, getDrizzleTableFromModel, rstoreUseDrizzle } from '../../utils'
+import type { RelationalQueryBuilder } from 'drizzle-orm/pg-core/query-builders/query'
+import { defineEventHandler, getQuery, getRouterParams } from 'h3'
+import { getDrizzleKeyWhere, getDrizzleTableFromModel, type RstoreDrizzleQueryParamsOne, rstoreUseDrizzle } from '../../utils'
 
 export default defineEventHandler(async (event) => {
   const { model: modelName, key } = getRouterParams(event) as { model: string, key: string }
+  const query = getQuery(event) as RstoreDrizzleQueryParamsOne
   const { table, primaryKeys } = getDrizzleTableFromModel(modelName)
 
-  const result = await rstoreUseDrizzle().select().from(table as any).where(getDrizzleKeyWhere(key, primaryKeys, table))
-  return result?.[0] ?? null
+  const dbQuery = rstoreUseDrizzle().query as unknown as Record<string, RelationalQueryBuilder<any, any>>
+  const result = await dbQuery[modelName].findFirst({
+    where: getDrizzleKeyWhere(key, primaryKeys, table),
+    with: query.with ? JSON.parse(query.with) : undefined,
+    columns: query.columns ? JSON.parse(query.columns) : undefined,
+  })
+  return result ?? null
 })
--- packages/nuxt-drizzle/src/runtime/server/api/index.get.ts
+++ packages/nuxt-drizzle/src/runtime/server/api/index.get.ts
@@ -1,5 +1,12 @@
+import type { RelationalQueryBuilder } from 'drizzle-orm/pg-core/query-builders/query'
+import { asc, desc } from 'drizzle-orm'
 import { createError, eventHandler, getQuery, getRouterParams } from 'h3'
 import { getDrizzleCondition, getDrizzleTableFromModel, type RstoreDrizzleQueryParams, rstoreUseDrizzle } from '../utils'
+
+const orderByOperators = {
+  asc,
+  desc,
+}
 
 export default eventHandler(async (event) => {
   const { model: modelName } = getRouterParams(event) as { model: string }
@@ -7,14 +14,16 @@ export default eventHandler(async (event) => {
 
   const query = getQuery(event) as RstoreDrizzleQueryParams
 
-  const q = rstoreUseDrizzle().select().from(table as any)
+  const dbQuery = rstoreUseDrizzle().query as unknown as Record<string, RelationalQueryBuilder<any, any>>
+
+  const q = {} as NonNullable<Parameters<typeof dbQuery[typeof modelName]['findMany']>[0]>
 
   if (query.where) {
     try {
       const where = JSON.parse(query.where as string) as any
       if (where) {
         const condition = getDrizzleCondition(table, where)
-        q.where(condition)
+        q.where = condition
       }
     }
     catch (e) {
@@ -27,12 +36,39 @@ export default eventHandler(async (event) => {
   }
 
   if (query.limit != null) {
-    q.limit(Number.parseInt(query.limit))
+    q.limit = Number.parseInt(query.limit)
   }
 
   if (query.offset != null) {
-    q.offset(Number.parseInt(query.offset))
+    q.offset = Number.parseInt(query.offset)
   }
 
-  return q.$dynamic()
+  if (query.with) {
+    q.with = JSON.parse(query.with)
+  }
+
+  if (query.columns) {
+    q.columns = JSON.parse(query.columns)
+  }
+
+  if (query.orderBy) {
+    const orderByData = typeof query.orderBy === 'string' ? [query.orderBy] : query.orderBy as Array<`${string}.${string}.${'asc' | 'desc'}`>
+    const orderBy = []
+    for (const rawOrderBy of orderByData) {
+      const parts = rawOrderBy.split('.')
+      if (parts.length !== 2) {
+        throw createError({
+          statusCode: 400,
+          statusMessage: 'Invalid orderBy',
+        })
+      }
+      const [columnName, order] = parts
+      const operator = orderByOperators[order as 'asc' | 'desc']
+      orderBy.push(operator((table as any)[columnName]))
+    }
+    q.orderBy = orderBy
+  }
+
+  const result = await dbQuery[modelName].findMany(q)
+  return result ?? []
 })
